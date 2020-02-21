f3664c3007e6dd257bdafdda963dd2c4705e0ab2
kevokvr <kvalenzu@msudenver.edu>
2020-02-20 17:01:24 -0700

    changes to gitignore

---
+++ routes/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    consumer_key:         'M3WWkN9jgw76zwlqZtoR3DIi2',
+    consumer_secret:      'MTTYdX5j4oZ4m2QjCTJa8C0ff176fyczMeqAt1nBZvxDdMIFsx',
+    access_token:         '626140531-aLSTdV8eHzQgW50mFigfKG2jrd5mBtrqggEqTPjP',
+    access_token_secret:  '1Gkbm9xJZU7I4lldP8KHP4fu5XIOl7zJBesstvsTZf4L2',
+    timeout_ms:           60*1000,  
+    strictSSL:            true,
+}
